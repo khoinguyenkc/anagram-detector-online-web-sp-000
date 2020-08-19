25e742fb1b893265f057c467a9022073f5a1435a
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-19 09:00:33 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+
+def initialization(word)
+  @word = word
+end
+
+def match(array)
+  array.each do | scrambledword |
+    sortedletters = scrambledword.split("").sort
+    if sortedletters == @word
+      return scrambledword
+    end
+  end
+
+end
