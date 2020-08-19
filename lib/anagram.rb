ae6fd68827aeb41aea0eba32b6845bb43c6db373
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-19 09:14:02 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,21 +7,17 @@ def initialize(word)
 end
 
 def match(array)
+  matches = []
+  sortedword = @word.split("").sort
 
   array.each do | scrambledword |
     sortedletters = scrambledword.split("").sort
-    sortedword = @word.split("").sort
-    binding.pry
     if sortedletters == sortedword
-
-      return scrambledword
+      matches << scrambledword
     end #end of if
-
   end #endof iteration
 
-  # i need to add a return nil. why? because if the if was neer activated, that means the scrambledword is never returned
-  #that means once the iteration is done, it wll return the array. and since the iteration is the last thing in this match method, whatever the iteration returns is whatever the match method return. and i don't want to return the array. either i return the correct word i return nil
-  return nil
+  return matches
 end
 
 end
